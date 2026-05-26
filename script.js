5a6e316abc764de07f2fffa22b232d54cef17446
2dstroy <109136890+2dstroy@users.noreply.github.com>
2026-05-26 21:55:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-auth.onAuthStateChanged(user => {
-    if (!user) {
-        window.location.href = "index.html";
-    } else {
-        console.log("Logged in as:", user.email);
-        
-        // Optional: Show hidden content
-        document.getElementById("protected-content").style.display = "block";
-    }
-});
+
