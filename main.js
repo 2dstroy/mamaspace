ac5bf8ff78ea0511a669c690accc9625c98f38ac
2dstroy <109136890+2dstroy@users.noreply.github.com>
2026-05-31 21:24:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ onAuthStateChanged(auth, (user) => {
     userInfo.innerHTML = `Welcome back, <b>@${username}</b>`;
   } else {
     alert("Please login first");
-    window.location.href = "index.html";
+    window.location.href = "login.html";
   }
 });
 
